c964fe45232233bf25d5eda1c26f19257803cebb
Dima <dima.tsymbal@outlook.com>
2023-07-25 18:08:19 +0300

    rebuild favorites start

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,13 +1,76 @@
+import * as React from 'react'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
 import FavList from './FavList'
 
-type Props = {}
-const Favorites = (props: Props) => {
+type TabPanelProps = {
+    children?: React.ReactNode
+    index: number
+    value: number
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props
+
     return (
-        <div style={{ width: '95%', margin: 'auto' }}>
-            <h2 style={{ textAlign: 'center' }}>Favorites</h2>
-            <br />
-            <FavList />
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
         </div>
     )
 }
-export default Favorites
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    }
+}
+
+const BasicTabs: React.FC = () => {
+    const [value, setValue] = React.useState(0)
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue)
+    }
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Box
+                sx={{
+                    borderColor: 'divider',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="basic tabs example"
+                >
+                    <Tab label="All" {...a11yProps(0)} />
+                    <Tab label="Mobiles" {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+                <FavList />
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+                Item Two
+            </CustomTabPanel>
+        </Box>
+    )
+}
+
+export default BasicTabs
