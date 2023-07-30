acecac4db6c1ab0b3f9df5929918f89515b8e0f1
Dima <dima.tsymbal@outlook.com>
2023-07-30 20:43:06 +0300

    fix mobilePage

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -7,21 +7,27 @@ import ImageDetails from 'pages/ImageDetails/ImageDetails'
 import Favorites from 'pages/Favorites/Favorites'
 import ScrollToTop from 'utils/scrollToTop'
 import Mobiles from 'pages/Mobiles/Mobiles'
+import { useAppSelector } from 'redux/hooks'
+import './App.css'
 
 type Props = {}
 
 function App(props: Props) {
+    const sunnyTheme = useAppSelector((state) => state.theme.sunnyTheme)
     return (
         <>
             <CssBaseline />
             <Header />
-            <ScrollToTop />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/image/:id" element={<ImageDetails />} />
-                <Route path="/favorites" element={<Favorites />} />
-                <Route path="/mobiles" element={<Mobiles />} />
-            </Routes>
+            <div className={sunnyTheme ? 'App' : 'App-dark'}>
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/image/:id" element={<ImageDetails />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                    <Route path="/mobiles" element={<Mobiles />} />
+                </Routes>
+            </div>
+
             <Footer />
         </>
     )
--- src/pages/Mobiles/Mobiles.tsx
+++ src/pages/Mobiles/Mobiles.tsx
@@ -1,10 +1,15 @@
+import { useAppSelector } from 'redux/hooks'
 import MobilesList from './MobilesList'
+import './Mobiles.scss'
 
 type Props = {}
 const Mobiles = (props: Props) => {
+    const sunnyTheme = useAppSelector((state) => state.theme.sunnyTheme)
     return (
         <div style={{ width: '95%', margin: 'auto' }}>
-            <h2 style={{ textAlign: 'center' }}>Mobiles wallpapers</h2>
+            <h2 className={sunnyTheme ? 'mobilesTitle' : 'mobilesTitle-dark'}>
+                Mobiles wallpapers
+            </h2>
             <MobilesList />
         </div>
     )
