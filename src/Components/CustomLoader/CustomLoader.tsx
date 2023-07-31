02f0da8e7a655c7d5266e77f536c3d56a6d744dc
Dima <dima.tsymbal@outlook.com>
2023-07-31 10:54:25 +0300

    fix loader

--- src/Components/CustomLoader/CustomLoader.tsx
+++ src/Components/CustomLoader/CustomLoader.tsx
@@ -1,6 +1,18 @@
+import { useAppSelector } from 'redux/hooks'
 import './CustomLoader.scss'
 type Props = {}
 const CustomLoader = (props: Props) => {
-    return <div className="custom-loader"></div>
+    const sunnyTheme = useAppSelector((state) => state.theme.sunnyTheme)
+    return (
+        <div
+            className={
+                sunnyTheme
+                    ? 'custom-loader-container'
+                    : 'custom-loader-container-dark'
+            }
+        >
+            <div className="custom-loader"></div>
+        </div>
+    )
 }
 export default CustomLoader
