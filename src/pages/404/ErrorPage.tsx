058dab1981849da3af23db9191bc194d204ea55c
Dima <dima.tsymbal@outlook.com>
2023-09-25 17:34:32 +0300

    change font and 404 styles

--- src/pages/404/ErrorPage.tsx
+++ src/pages/404/ErrorPage.tsx
@@ -1,10 +1,16 @@
+import { useAppSelector } from 'redux/hooks'
 import './ErrorPage.scss'
 
 type Props = {}
 
 const ErrorPage = (props: Props) => {
+    const sunnyTheme = useAppSelector((state) => state.theme.sunnyTheme)
     return (
-        <div className="error404-container">
+        <div
+            className={
+                sunnyTheme ? 'error404-container' : 'error404-container_dark'
+            }
+        >
             <h1>404 - Page Not Found</h1>
             <p>Oops! The page you are looking for does not exist.</p>
         </div>
